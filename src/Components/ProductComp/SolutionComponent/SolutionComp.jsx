01416ef9b574dcd59ab71e5bfde576cf05014020
suresh.manivannan <suresh.manivannan@e-consystems.com>
2024-11-19 16:02:16 +0530

    solution comp vedio added

--- src/Components/ProductComp/SolutionComponent/SolutionComp.jsx
+++ src/Components/ProductComp/SolutionComponent/SolutionComp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './SolutionComp.css'; 
-import ideaimg from "../../../assets/productpage/idea.png";
+import './SolutionComp.css';
+import product from "../../../assets/productpage/product-video.mp4"; // Replace with your video file path
 
 const SolutionComp = () => {
   return (
@@ -10,8 +10,17 @@ const SolutionComp = () => {
 
         <div className="solution-content">
           <div className="solution-left-section">
-            <div className="solution-image">
-              <img src={ideaimg} alt="Center Image" className="bulb-image" />
+            <div className="solution-video">
+              <video 
+                className="video-player" 
+                autoPlay 
+                loop 
+                muted 
+                playsInline 
+              >
+                <source src={product} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
             </div>
           </div>
 
@@ -24,12 +33,10 @@ const SolutionComp = () => {
                 <h3> <span className="checkmark">✓</span>Superior Accuracy and Consistency</h3>
               </div>
               <div className="solution-item">
-               
                 <h3> <span className="checkmark">✓</span>Ease of Integration and Scalability</h3>
               </div>
               <div className="solution-item">
-                
-                <h3><span className="checkmark">✓</span>Unmatched Reliability and Uptime</h3>
+                <h3> <span className="checkmark">✓</span>Unmatched Reliability and Uptime</h3>
               </div>
               <div className="solution-item">
                 <h3> <span className="checkmark">✓</span>Uninterrupted Shopping Experience</h3>
