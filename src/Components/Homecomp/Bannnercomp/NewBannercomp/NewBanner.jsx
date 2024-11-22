f20de7fa259cdb36316aeb8252c67c21ef4a30cc
suresh.manivannan <suresh.manivannan@e-consystems.com>
2024-11-22 14:54:25 +0530

    banner changes

---
+++ src/Components/Homecomp/Bannnercomp/NewBannercomp/NewBanner.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './NewBanner.css';
+import newmobile from "../../../../assets/homepage/newhandimg.png";
+import mobilerack from "../../../../assets/homepage/mobilerack.png";
+import Modelbutton from "../../../ButtonComp/Modelbutton";
+
+const NewBanner = () => {
+  return (
+    <div style={{ backgroundColor: "#1050c1" }}>
+      <div className="mainContainer">
+        <div className="NewBanner-bannerContainer">
+          <div className="NewBanner-bannerLeft">
+            <div className="NewBanner-imageRow">
+              <img src={newmobile} className="NewBanner-mobileImg" alt="Mobile" />
+              <img src={mobilerack} className="NewBanner-mobileRack" alt="Rack" />
+            </div>
+          </div>
+          <div className="NewBanner-bannerRight">
+            <h3>SHELFTrack – Eyes on Every Shelf</h3>
+            <div className="NewBanner-spacer"></div>
+            <h2>
+              Captures real-time shelf images, turning visibility into actionable insights for smarter retail management
+            </h2>
+            <div className="NewBanner-spacerLarge"></div>
+            <Modelbutton
+              text="Contact Us"
+              backgroundColor="#ffc107"
+              textColor="#131b2d"
+              hoverTextColor="#344ea1"
+              className="NewBanner-bannerButton"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewBanner;
