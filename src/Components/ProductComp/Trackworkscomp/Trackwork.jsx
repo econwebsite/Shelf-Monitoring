c0ed120753a81f64dd440ec3b30bb8e383ff3743
suresh.manivannan <suresh.manivannan@e-consystems.com>
2024-11-27 15:14:54 +0530

    img changes

--- src/Components/ProductComp/Trackworkscomp/Trackwork.jsx
+++ src/Components/ProductComp/Trackworkscomp/Trackwork.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Trackwork.css';
-import shelftrack1 from ".././../../assets/productpage/shelftrack1.png"
-import shelftrack2 from ".././../../assets/productpage/shelftrack2.png"
-import shelftrack3 from ".././../../assets/productpage/shelftrack3.png"
+// import shelftrack1 from ".././../../assets/productpage/shelftrack1.png"
+// import shelftrack2 from ".././../../assets/productpage/shelftrack2.png"
+// import shelftrack3 from ".././../../assets/productpage/shelftrack3.png"
+import howshelf from "../../../assets/productpage/howshelfworks.jpg"
 
 const Trackwork = () => {
   return (
@@ -14,9 +15,10 @@ const Trackwork = () => {
 
       <div className="Trackwork-blue-box">
         <div className="Trackwork-image-container">
-          <img src={shelftrack1} alt="Image 1" className="Trackwork-img1" data-aos="fade-right" data-aos-duration="1300"/>
+          {/* <img src={shelftrack1} alt="Image 1" className="Trackwork-img1" data-aos="fade-right" data-aos-duration="1300"/>
           <img src={shelftrack2} alt="Image 2" className="Trackwork-img2" data-aos="zoom-in" data-aos-duration="1300" />
-          <img src={shelftrack3} alt="Image 3" className="Trackwork-img3" data-aos="fade-left" data-aos-duration="1300"/>
+          <img src={shelftrack3} alt="Image 3" className="Trackwork-img3" data-aos="fade-left" data-aos-duration="1300"/> */}
+          <img src={howshelf}></img>
         </div>
         <div className="Trackwork-aqua-box">
           <div className="Trackwork-border-box">
